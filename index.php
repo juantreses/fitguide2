ffad3e9763f92ba42524cce7a7d7c791f628260b
juantreses <jandries@live.be>
2019-12-23 15:10:12 +0100

    index klaar (nog niet responsief)

--- index.php
+++ index.php
@@ -15,14 +15,14 @@ PrintNavBar();
             <div class="overlay">
                 <h1>Fitguide</h1>
                 <p>Progress starts here</p>
-            </div>
-        </div>
+            </div> <!-- overlay -->
+        </div> <!-- pic -->
     </section>
     <section class="section-two">
         <div class="pic">
             <h2>Our promise</h2>
             <img src="images/boxjump.jpg" alt="">
-        </div>
+        </div> <!-- pic -->
         <article>
             <span class="fa fa-check-square"></span>
             <h3>Improve performance</h3>
@@ -35,7 +35,57 @@ PrintNavBar();
             <p>A lasting impact</p>
         </article>
     </section>
+    <section class="section-three">
+        <div class="pic">
+            <div class="overlay">
+                <h2>Get your guide today</h2>
+                <a href="register.php"><button>Register</button></a>
+            </div> <!-- overlay -->
+            <img src="images/dumbbells.jpg" alt="">
+        </div> <!-- pic -->
+    </section>
+    <section class="section-four" id="contact">
+        <img src="images/fitsmile.jpg" alt="">
+        <form action="lib/contact.php" method="post">
+            <h2>Contact</h2>
+            <input type="hidden" id="formname" name="formname" value="contact_form">
+            <input type="hidden" id="tablename" name="tablename" value="fitguideContact">
+            <ul>
+                <li>
+                    <label for="con_naam"></label>
+                    <input type="text" id="con_naam" name="con_naam" placeholder="Name">
+                </li>
+                <li>
+                    <label for="con_email"></label>
+                    <input type="email" id="con_email" name="con_email" placeholder="Email">
+                </li>
+                <li>
+                    <label for="con_subject"></label>
+                    <input type="text" id="con_subject" name="con_subject" placeholder="Subject">
+                </li>
+                <li>
+                    <label for="con_vraag"></label>
+                    <textarea name="con_vraag" id="con_vraag" rows="5" placeholder="Type your message here..."></textarea>
+                </li>
+                <li>
+                    <input type="submit" value="Submit">
+                </li>
+            </ul>
+            <p>Thanks for submitting!</p>
+        </form>
+    </section>
 </main>
+<footer>
+    <p>&copy; 2019 by FitGuide</p>
+</footer>
+<a href="javascript:void(0);" id="goToTop">
+    <span class="fa fa-arrow-circle-up"></span>
+</a>
+<!-- jQuery library -->
+<script
+        src="https://code.jquery.com/jquery-3.4.1.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+        crossorigin="anonymous"></script>
 <!-- Eigen scripts -->
 <script src="js/main.js"></script>
 </body>
--- js/main.js
+++ js/main.js
@@ -7,3 +7,67 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.display = "none";
 }
+
+window.onscroll = function() {
+        document.getElementById("myNav").style.display = "none";
+}
+
+//Smooth scrolling
+
+$('a[href*="#"]')
+// Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+        // On-page links
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            &&
+            location.hostname == this.hostname
+        ) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, function() {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+            }
+        }
+    });
+
+
+//Scroll To Top
+$(document).ready(function(){
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#goToTop').fadeIn();
+        } else {
+            $('#goToTop').fadeOut();
+        }
+    });
+    var duration_ms = 800;
+    $('#goToTop').click(function () {
+        //var last_scroll = $(window).scrollTop();
+        $('html, body').animate({ scrollTop: 0 }, duration_ms, function(){
+            setTimeout(function(){
+                $('html, body').animate({ scrollTop: last_scroll }, duration_ms);
+            }, duration_ms);
+        });
+    });
+});
+
