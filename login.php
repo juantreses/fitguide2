4be2b7361903a0e8f1bf51204f4f1daa1a2da541
jensdebacker <58468022+jensdebacker@users.noreply.github.com>
2019-12-31 16:28:28 +0100

    Add files via upload

--- login.php
+++ login.php
@@ -2,13 +2,18 @@
 $login = true;
 require_once "lib/autoload.php";
 
+
 //redirect naar homepage als de gebruiker al ingelogd is
 if ( isset($_SESSION['usr']) ) { $_SESSION["msg"][] = "U bent al ingelogd!"; header("Location: index.php"); exit; }
-
-BasicHead();
 ShowMessages();
+BasicHead();
+
 ?>
 
+<body>
+<?php
+PrintNavBar();
+?>
 <main>
     <?php
     print LoadTemplate("login");
