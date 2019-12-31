08a6d116b9e77d55dbedaaad67e7dad33034afff
jensdebacker <58468022+jensdebacker@users.noreply.github.com>
2019-12-31 16:23:44 +0100

    Add files via upload

--- lib/show_messages.php
+++ lib/show_messages.php
@@ -1,14 +1,16 @@
 <?php
 function ShowMessages()
 {
+    if ( ! $_SESSION["head_printed"] ) BasicHead();
     //weergeven messages
-    if ( isset($_SESSION["msg"]) )
+    if ( count($_SESSION["msg"]) > 0)
     {
         foreach( $_SESSION["msg"] as $message )
         {
             $row = array( "message" => $message );
+            $data = array( $row ) ;
             $templ = LoadTemplate("message");
-            print ReplaceContentOneRow( $row, $templ );
+            print ReplaceContent( $data, $templ );
         }
 
         unset($_SESSION['msg']);
