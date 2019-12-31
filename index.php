675964857f62c32ec31401ecd936440ac50c8839
jensdebacker <58468022+jensdebacker@users.noreply.github.com>
2019-12-31 16:28:41 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $index = true;
 require_once "lib/autoload.php";
-
+ShowMessages();
 BasicHead();
 ?>
 <body>
@@ -84,9 +84,7 @@ PrintNavBar();
 <a href="javascript:void(0);" id="goToTop">
     <span class="fa fa-arrow-circle-up"></span>
 </a>
+
 <!-- jQuery library -->
-<script
-    src="https://code.jquery.com/jquery-3.4.1.min.js"
-    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-    crossorigin="anonymous"></script>
+
 </body>
